c43594974f86f541c46316b871d5ff3d8c8965ca
Samiksha Garg <gargsamiksha0306@gmail.com>
2022-02-26 01:55:42 +0530

    added hashed pass and login route

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require('bcrypt');
 
 //REGISTER 
 
@@ -16,13 +17,15 @@ router.post("/register", async(req, res) => {
             res.status(400).json({message : {msgBody : "Email already in use", msgError : true}});
         }
         else {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPass = await bcrypt.hash(password, salt);
             try {
                 const newUser = new User({
-                    name : req.body.name,
-                    email : req.body.email,
-                    password : req.body.password,
-                    dob : new Date(req.body.dob),
-                    role : req.body.role
+                    name : name,
+                    email : email,
+                    password : hashedPass,
+                    dob : new Date(dob),
+                    role : role
                 });
         
                 const user = await newUser.save();
@@ -35,5 +38,30 @@ router.post("/register", async(req, res) => {
     })
    
 });
+
+//LOGIN 
+
+router.post("/login", async (req, res) => {
+    try {
+      const user = await User.findOne({ email : req.body.email });
+
+      if (!user) {
+        return res.status(400).json("No such user exits");
+      }
+  
+      const validated = await bcrypt.compare(req.body.password, user.password);
+
+        if (!validated) {
+        return res.status(400).json("Wrong credentials!");
+      }
+  
+      const { password, ...others } = user._doc;
+      return res.status(200).json(others);
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  });
+
+
 
 module.exports = router;
