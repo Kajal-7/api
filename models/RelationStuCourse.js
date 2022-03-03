7deab3d90ad37aa4df09c78ddbe35fbfdd1d2614
Samiksha Garg <gargsamiksha0306@gmail.com>
2022-03-03 23:40:51 +0530

    added rel schema

---
+++ models/RelationStuCourse.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const relationStuCourseSchema = new mongoose.Schema({
+    studentId : {
+        type : String,
+        required : true,
+    },
+    courseId : {
+        type : String,
+        required : true,
+    },
+    attended : {
+        type : Number,
+        default : 0
+    },
+    totalClasses : {
+        type : Number,
+        default : 0
+    }
+},{timestamps: true});
+
+relationStuCourseSchema.index({ studentId: 1, courseId: 1}, { unique: true });
+
+module.exports = mongoose.model("RelStuCourse", relationStuCourseSchema);
