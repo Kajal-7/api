7709725f67e60b35d1ae14c27de92080cf5b0339
Samiksha Garg <gargsamiksha0306@gmail.com>
2022-03-09 23:03:01 +0530

    adding personal events

--- routes/auth.js
+++ routes/auth.js
@@ -62,6 +62,23 @@ router.post("/login", async (req, res) => {
     }
   });
 
+  router.put("/update/:id", async (req, res) => {
+    try {
+      console.log(req.body)
+        const updatedUser = await User.findByIdAndUpdate(
+          req.params.id, {
+            $set: req.body
+          },
+          {new : true}
+        );
+      return res.status(200).json(updatedUser);
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  });
+
+  
+
 
 
 module.exports = router;
--- routes/courses.js
+++ routes/courses.js
@@ -7,7 +7,7 @@ router.get("/:id", async (req, res) => {
     try {
         const course = await Course.findById(req.params.id);
         res.status(200).json(course);
-    } catch {
+    } catch (err) {
         res.status(500).json(err);
     }
 });
