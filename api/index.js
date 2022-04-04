8bc28b97f5005ff3c602e314bec439de67b1bf6d
Kajal <kajalkaushal700@gmail.com>
2022-04-05 00:03:58 +0530

    Added route for relationStuCourse

--- api/index.js
+++ api/index.js
@@ -10,6 +10,7 @@ const classRoute = require("./routes/classes");
 const chatRoute = require("./routes/chat");
 const messageRoute = require("./routes/messages");
 const materialRoute = require("./routes/material");
+const relationStuCourseRoute= require("./routes/relationStuCourse");
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json({limit: '50mb'}));
@@ -31,6 +32,8 @@ app.use("/api/class", classRoute);
 app.use("/api/chat", chatRoute);
 app.use("/api/message", messageRoute);
 app.use("/api/material", materialRoute);
+app.use("/api/relationStuCourse", relationStuCourseRoute);
+
 
 app.listen(1000, function(){
     console.log("Backend Running on port 1000");
